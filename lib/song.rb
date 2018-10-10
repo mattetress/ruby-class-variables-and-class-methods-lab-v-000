c32f2d55840edd34ad177e964f8a8c6b6545b9d2
Matt Etress <mattetress@me.com>
2018-10-10 01:47:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,7 @@ class Song
   def self.genre_count
     genre_hash = {}
     genres.each do |genre|
-      if !genres.keys.include?(genre)
+      if !genre_hash.keys.include?(genre)
         genre_hash[genre] = 1
       else
         genre_hash[genre] += 1
